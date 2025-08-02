ceabbc02fb409d7ab10f5b82a7fb6a3d3e7f07f5
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2025-08-02 11:07:28 +0200

    Implement successful crossing
    Increase car speed

--- car_manager.py
+++ car_manager.py
@@ -1,6 +1,8 @@
 from turtle import Turtle
 import random
 
+from player import MOVE_DISTANCE
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
@@ -27,3 +29,6 @@ class CarManager(Turtle):
     def move_cars(self):
         for car in self.all_cars:
             car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
--- main.py
+++ main.py
@@ -29,4 +29,9 @@ while game_is_on:
             print("game over")
             game_is_on = False
 
+    #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+
 screen.exitonclick()
